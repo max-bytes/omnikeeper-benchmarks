a293047026c4a5b97931a79177831a8bb85f650d
github-action-benchmark <github@users.noreply.github.com>
2022-09-06 15:26:21 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 1a2b69dc1ccd52b12d8575297e0790d4494e227f

---
+++ data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1662477981949,
+  "repoUrl": "https://github.com/max-bytes/omnikeeper",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Maximilian Csuk",
+            "username": "maximiliancsuk",
+            "email": "maximilian.csuk@gmx.at"
+          },
+          "committer": {
+            "name": "Maximilian Csuk",
+            "username": "maximiliancsuk",
+            "email": "maximilian.csuk@gmx.at"
+          },
+          "id": "1a2b69dc1ccd52b12d8575297e0790d4494e227f",
+          "message": "bugfix",
+          "timestamp": "2022-09-06T15:24:48Z",
+          "url": "https://github.com/max-bytes/omnikeeper/commit/1a2b69dc1ccd52b12d8575297e0790d4494e227f"
+        },
+        "date": 1662477981715,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "PerfTests.GetMergedAttributesTest.GetMergedAttributes(CIIDSelection: \"all\", AttributeValueType: \"Text\", EnablePerRequestModelCaching: False, UseLatestTable: True, PreSetupData: True, AttributeCITuple: (1000, 10000, 4, 50))",
+            "value": 128798727.9,
+            "unit": "ns",
+            "range": "± 7209462.34162403"
+          }
+        ]
+      }
+    ]
+  }
+}
